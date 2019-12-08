0d056939835dcff87157da149228de1cc02f7c39
Lauren Zou <zou.lauren@gmail.com>
2019-12-08 02:41:30 -0500

    Make buzzer more mobile-friendly.

--- src/client/play.ts
+++ src/client/play.ts
@@ -4,16 +4,21 @@ const bodyElement = document.getElementsByTagName('body').item(0);
 
 let quackSound = new Audio('quack.mp3');
 let quackAltSound = new Audio('quack-alt.mp3');
+let isTouchDevice = 'ontouchstart' in document.documentElement;
 
 for (let buzzer of buzzers) {
-  buzzer.addEventListener('mousedown', function(e) {
+  buzzer.addEventListener(isTouchDevice ? 'touchstart' : 'mousedown', function(
+    e
+  ) {
+    buzzer.classList.add('active');
     htmlElement.classList.add('buzzer-active');
     bodyElement.classList.add('buzzer-active');
     let sound = Math.floor(Math.random() * 10) < 1 ? quackAltSound : quackSound;
     sound.play();
     sound.currentTime = 0;
   });
-  buzzer.addEventListener('mouseup', function(e) {
+  buzzer.addEventListener(isTouchDevice ? 'touchend' : 'mouseup', function(e) {
+    buzzer.classList.remove('active');
     htmlElement.classList.remove('buzzer-active');
     bodyElement.classList.remove('buzzer-active');
   });
