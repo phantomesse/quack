ad88a334c1fdd18d3992f1006bdcdc6629de74a8
Lauren Zou <zou.lauren@gmail.com>
2019-12-08 03:02:26 -0500

    Remove incorrectly compiled files.

--- src/server/team.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Team = /** @class */ (function () {
-    function Team() {
-        this.points = 0;
-    }
-    return Team;
-}());
-exports["default"] = Team;
